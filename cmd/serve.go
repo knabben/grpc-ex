8db1aa758e3585486667049e3b9778418d800aac
Amim Knabben <amim@loadsmart.com>
2017-12-31 08:37:37 -0200

    Serve command

--- cmd/serve.go
+++ cmd/serve.go
@@ -1,10 +1,7 @@
 package cmd
 
 import (
-	"crypto/tls"
 	"fmt"
-	"io"
-	"net"
 	"net/http"
 	"strings"
 
@@ -13,89 +10,68 @@ import (
 	"github.com/spf13/cobra"
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials"
 )
 
-var serveCmd = &cobra.Command{
-	Use:   "serve",
-	Short: "A brief description of your command",
-	Run: func(cmd *cobra.Command, args []string) {
-		serve()
-	},
-}
+var (
+	remoteGrpc string
+	port       int
 
-type server struct{}
-
-func newServer() pb.DamageServiceServer {
-	return new(server)
-}
-
-func (s *server) Damage(ctx context.Context, msg *pb.DamageMessage) (*pb.DamageMessage, error) {
-	return &pb.DamageMessage{Value: msg.Value}, nil
-}
+	serveCmd = &cobra.Command{
+		Use:   "serve",
+		Short: "A brief description of your command",
+		Run: func(cmd *cobra.Command, args []string) {
+			serve()
+		},
+	}
+)
 
 func init() {
+	serveCmd.Flags().StringVar(&remoteGrpc, "grpc", "localhost:9090", "Remote GRPC address")
+	serveCmd.Flags().IntVar(&port, "port", 8080, "Local port")
 	rootCmd.AddCommand(serveCmd)
 }
 
-func grpcHandlerFunc(grpcServer *grpc.Server, otherHandler http.Handler) http.Handler {
+func allowCORS(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.ProtoMajor == 2 && strings.Contains(r.Header.Get("Content-Type"), "application/grpc") {
-			fmt.Println("grpc")
-			grpcServer.ServeHTTP(w, r)
-		} else {
-			fmt.Println("http")
-			otherHandler.ServeHTTP(w, r)
+		if origin := r.Header.Get("Origin"); origin != "" {
+			w.Header().Set("Access-Control-Allow-Origin", origin)
+			if r.Method == "OPTIONS" && r.Header.Get("Access-Control-Request-Method") != "" {
+				preflightHandler(w, r)
+				return
+			}
 		}
+		h.ServeHTTP(w, r)
 	})
 }
 
+func preflightHandler(w http.ResponseWriter, r *http.Request) {
+	headers := []string{"Content-Type", "Accept"}
+	w.Header().Set("Access-Control-Allow-Headers", strings.Join(headers, ","))
+	methods := []string{"GET", "HEAD", "POST", "PUT", "DELETE"}
+	w.Header().Set("Access-Control-Allow-Methods", strings.Join(methods, ","))
+	fmt.Println("preflight request for %s", r.URL.Path)
+	return
+}
+
 func serve() error {
-	opts := []grpc.ServerOption{
-		grpc.Creds(credentials.NewClientTLSFromCert(demoCertPool, "localhost:10000"))}
+	fmt.Printf("Access remote GRPC on %s\n", remoteGrpc)
+	fmt.Printf("Listening on port %d\n", port)
 
-	grpcServer := grpc.NewServer(opts...)
-	pb.RegisterDamageServiceServer(grpcServer, newServer())
 	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
 
-	dcreds := credentials.NewTLS(&tls.Config{
-		ServerName: demoAddr,
-		RootCAs:    demoCertPool,
-	})
 	mux := http.NewServeMux()
 	gwmux := runtime.NewServeMux()
 
-	dopts := []grpc.DialOption{grpc.WithTransportCredentials(dcreds)}
+	dialOpts := []grpc.DialOption{grpc.WithInsecure()}
 	err := pb.RegisterDamageServiceHandlerFromEndpoint(
-		ctx, gwmux, demoAddr, dopts)
-	if err != nil {
-		fmt.Printf("serve %v\n", err)
-		return err
-	}
-
-	mux.Handle("/", gwmux)
-	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
-		io.WriteString(w, "OK\n")
-	})
-
-	conn, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+		ctx, gwmux, remoteGrpc, dialOpts)
 	if err != nil {
 		panic(err)
 	}
 
-	srv := &http.Server{
-		Addr:    demoAddr,
-		Handler: grpcHandlerFunc(grpcServer, mux),
-		TLSConfig: &tls.Config{
-			Certificates: []tls.Certificate{*demoKeyPair},
-			NextProtos:   []string{"h2"},
-		},
-	}
+	mux.Handle("/", gwmux)
 
-	fmt.Printf("grpc on port: %d\n", port)
-	if err = srv.Serve(tls.NewListener(conn, srv.TLSConfig)); err != nil {
-		fmt.Println("ListenAndServe: ", err)
-		return nil
-	}
-	return nil
+	return http.ListenAndServe(fmt.Sprintf(":%d", port), allowCORS(mux))
 }
